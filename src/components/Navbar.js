252f43fff078bc6e93bb29b6051009df44fa1b5e
Firdevs <firdes.sahin92@gmail.com>
2025-02-10 11:56:46 +0300

    Add Navbar component with navigation links

---
+++ src/components/Navbar.js
@@ -0,0 +1,28 @@
+"use client";
+import React, { useState } from "react";
+import navbarLinks from "@/config/navbar";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function Navbar() {
+  const pathname = usePathname();
+
+  return (
+    <nav className="hidden lg:flex flex-row py-4 px-3 gap-[30px] outline outline-4 outline-black-500 rounded-md">
+      {navbarLinks.map((navlink) => {
+        const activeLink = pathname === navlink.path;
+        return (
+          <Link
+            key={navlink.id}
+            href={navlink.path}
+            className={`${
+              activeLink ? "bg-black-600" : "text-gray-400"
+            } desktop:text-lg xl:text-sm px-4 py-2 cursor-pointer`}
+          >
+            {navlink.name}
+          </Link>
+        );
+      })}
+    </nav>
+  );
+}
---
+++ src/config/navbar.js
@@ -0,0 +1,8 @@
+const navbarLinks = [
+  { id: 1, name: "Home", path: "/" },
+  { id: 2, name: "Movies & Shows", path: "/movies" },
+  { id: 3, name: "Support", path: "/support" },
+  { id: 4, name: "Subscribe", path: "/subscribe" },
+];
+
+export default navbarLinks;
