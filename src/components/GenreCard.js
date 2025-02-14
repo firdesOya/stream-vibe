adbf21c0aadd37a36d0dff84fe7798fa3e749540
Firdevs <firdes.sahin92@gmail.com>
2025-02-15 00:38:23 +0300

    feat: Update GenreCard component layout and styling for improved movie display

--- src/components/GenreCard.js
+++ src/components/GenreCard.js
@@ -2,24 +2,32 @@ import Link from "next/link";
 import React from "react";
 
 export default function GenreCard({ data }) {
+  // const genreMovies = data.movies.filter((movie) =>
+  //   movie.genre_ids.includes(data.id)
+  // );
   
+  // const selectedMovies = genreMovies.slice(0, 4);
   return (
     <Link
       href={`/movies/genres/${data.id}`}
-      className="block p-4 bg-blue-600 rounded-lg text-center hover:bg-blue-700 transition"
+      className="block h-[260px] xl:[360px] p-4 bg-black-600  rounded-lg text-center border border-black-400 transition"
     >
-      <h3 className="text-white font-bold">{data.name}</h3>
-      <div className="grid grid-cols-2 gap-2 mt-4">
-        {data.movies?.slice(0, 4).map((movie) => (
-          <div key={movie.id} className="relative">
+      <div className="grid grid-cols-2 grid-rows-2 gap-1 ">
+        {data.movies?.map((movie) => (
+          <div key={movie.id}>
             <img
               src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
               alt={movie.title}
-              className="w-full h-32 object-cover rounded-md absolute"
+              className="w-full h-[98px] object-cover rounded-md "
             />
-            <p className="text-xs text-white mt-1 text-center">{movie.title}</p>
           </div>
         ))}
+      </div>
+      <div className="flex justify-between items-center w-full ">
+        <h3 className="text-white font-bold">{data.name}</h3>
+        <button>
+          <img src="/icons/right-arrow.svg" />
+        </button>
       </div>
     </Link>
   );
