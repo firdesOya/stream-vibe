a2c22ea92971c791faa517339c6e62d8dc4ce17e
Firdevs <firdes.sahin92@gmail.com>
2025-02-15 00:38:54 +0300

    feat: Optimize imports in MovieSection component by removing unused useEffect and useState

--- src/components/MovieSection.js
+++ src/components/MovieSection.js
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useRef } from "react";
 import MovieCard from "./MovieCard";
 import GenreCard from "./GenreCard";
 import SliderButtons from "./SliderButtons";
