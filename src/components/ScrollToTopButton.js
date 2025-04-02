36337b1714669b3b74cd9ec6760212843f41c3cd
Firdevs <firdes.sahin92@gmail.com>
2025-04-02 19:53:19 +0300

    feat: Add pulse animation to ScrollToTopButton for improved visibility

--- src/components/ScrollToTopButton.js
+++ src/components/ScrollToTopButton.js
@@ -20,7 +20,7 @@ export default function ScrollToTopButton() {
       onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
       className={` ${
         isVisible ? "opacity-100" : "opacity-0 pointer-events-none"
-      } w-12 h-12 fixed z-10 right-10 bottom-10 desktop:left-1/2 desktop:-translate-x-1 desktop:ml-[750px] flex items-center justify-center cursor-pointer bg-black-800 border border-black-200 rounded-full rotate-90`}
+      } w-12 h-12 fixed z-10 right-10 bottom-10  animate-pulse desktop:left-1/2 desktop:-translate-x-1 desktop:ml-[750px] flex items-center justify-center cursor-pointer bg-black-800 border border-black-200 rounded-full rotate-90`}
     >
       <img src="/icons/left-arrow.svg" className="w-full h-full p-2" />
     </button>
