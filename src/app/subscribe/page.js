14536350e5037470932908d2c83940a91663bd1c
Firdevs <firdes.sahin92@gmail.com>
2025-04-21 16:58:41 +0300

    feat: Add SubscribePage and SupportPage components with FreeTrail integration

---
+++ src/app/subscribe/page.js
@@ -0,0 +1,11 @@
+
+import FreeTrail from '@/components/FreeTrail'
+import React from 'react'
+
+export default function SubcribePage() {
+  return (
+    <div>
+    <FreeTrail/>
+    </div>
+  )
+}
---
+++ src/app/support/page.js
@@ -0,0 +1,12 @@
+import FreeTrail from "@/components/FreeTrail";
+import QuestionSection from "@/components/QuestionSection";
+import React from "react";
+
+export default function SupportPage() {
+  return (
+    <div>
+      <QuestionSection />
+      <FreeTrail />
+    </div>
+  );
+}
