7d420c9a395f0d998800e5463b839975aed43944
Firdevs <firdes.sahin92@gmail.com>
2025-04-24 21:54:42 +0300

    feat: Add PlanSection and PlanOverview components to SubscribePage for improved layout

--- src/app/subscribe/page.js
+++ src/app/subscribe/page.js
@@ -1,10 +1,14 @@
 
 import FreeTrail from '@/components/FreeTrail'
+import PlanOverview from '@/components/PlanOverview'
+import PlanSection from '@/components/PlanSection'
 import React from 'react'
 
 export default function SubcribePage() {
   return (
     <div>
+    <PlanSection/>
+    <PlanOverview/>
     <FreeTrail/>
     </div>
   )
