6729b9b24066cec7bd78bb0615b27d69580d12e4
Firdevs <firdes.sahin92@gmail.com>
2025-04-24 15:55:45 +0300

    feat: Adjust image height in SupportIntro and update margin in SupportSection for improved layout

--- src/components/SupportSection.js
+++ src/components/SupportSection.js
@@ -4,7 +4,7 @@ import SupportContactForm from './SupportContactForm'
 
 export default function SupportSection() {
   return (
-    <div className='flex flex-col lg:flex-row w-full justify-between gap-12 2xl:gap-14'>
+    <div className='flex flex-col lg:flex-row w-full justify-between gap-12 2xl:gap-14 mt-10 lg:mt-24 2xl:mt-28'>
       <SupportIntro/>
       <SupportContactForm/>
     </div>
